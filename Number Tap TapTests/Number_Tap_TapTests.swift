daccdc7770f95ddd5448eed889f98e7fc3560d78
Akshay Yadav <akshayadav@gmail.com>
2015-03-20 23:19:22 -0500

    Initial Commit

---
+++ Number Tap TapTests/Number_Tap_TapTests.swift
@@ -0,0 +1,36 @@
+//
+//  Number_Tap_TapTests.swift
+//  Number Tap TapTests
+//
+//  Created by Akshay Yadav on 3/20/15.
+//  Copyright (c) 2015 Akshay Yadav. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Number_Tap_TapTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
